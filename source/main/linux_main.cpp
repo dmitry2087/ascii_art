e93b5d8880b6f26e780952617dcd35ace2afd137
dmitry2087 <dmitry2087@gmail.com>
2025-07-12 21:37:07 +0700

    Delete source/main folder

--- source/main/linux_main.cpp
+++
@@ -1,9 +0,0 @@
-//     #           "      m                   mmmm   mmmm   mmmm  mmmmmm
-//  mmm#  mmmmm  mmm    mm#mm   m mm  m   m  "   "# m"  "m #    #     #"
-// #" "#  # # #    #      #     #"  " "m m"      m" #  m # "mmmm"    m" 
-// #   #  # # #    #      #     #      #m#     m"   #    # #   "#   m"  
-// "#m##  # # #  mm#mm    "mm   #      "#    m#mmmm  #mm#  "#mmm"  m"   
-//                                     m"                               
-// ASCII-ART app for Linux                              
-
-// In Developing.
